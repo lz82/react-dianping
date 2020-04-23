5d06a5a771fb9b50e7ce0820b55ecee39b05247a
lz82 <nickylau82@gmail.com>
2020-04-23 22:56:20 +0800

    :construction: complete likes style

--- src/pages/home/components/likes/index.jsx
+++ src/pages/home/components/likes/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import css from './index.module.less';
 
@@ -10,9 +11,23 @@ export default function Likes(props) {
       <div className={css['header']}>猜你喜欢</div>
       <div className={css['list']}>
         {data.map((item) => (
-          <div key={item.id} className={css['item']}>
-            <img src={item.picture} alt="" />
-          </div>
+          <Link key={item.id} className={css['item']} to={`/detail/${item.id}`}>
+            <div className={css['pic-container']}>
+              <div className={css['pic-tag']}>{item.tag}</div>
+              <img alt="" className={css['pic']} src={item.picture} />
+            </div>
+            <div className={css['content']}>
+              <div className={css['shop']}>{item.shop}</div>
+              <div className={css['product']}>{item.product}</div>
+              <div className={css['detail']}>
+                <div className={css['price']}>
+                  <ins className={css['current-price']}>{item.currentPrice}</ins>
+                  <del className={css['old-price']}>{item.oldPrice}</del>
+                </div>
+                <div className={css['sale']}>{item.saleDesc}</div>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
