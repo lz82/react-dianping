aa5acdb9c6274476edebd7ce76b29a4d9960811a
lz82 <nickylau82@gmail.com>
2020-05-03 20:08:31 +0800

    :construction: modify router

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,64 +3,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PrivateRoute from '@/components/private-route';
 import ScrollToTop from '@/components/scroll-to-top';
-import loadable from '@loadable/component';
-import Loading from '@/components/loading';
 
-const Login = loadable(() => import('@/pages/login'), {
-  fallback: (
-    <div>
-      <Loading />
-    </div>
-  )
-});
+import lazyLoad from '@/utils/lazy-load';
 
-const Home = loadable(() => import('@/pages/home'), {
-  fallback: (
-    <div>
-      <Loading />
-    </div>
-  )
-});
+const Login = lazyLoad(import('@/pages/login'));
+const Home = lazyLoad(import('@/pages/home'));
 
-const CouponDetail = loadable(() => import('@/pages/coupon-detail'), {
-  fallback: (
-    <div>
-      <Loading />
-    </div>
-  )
-});
+const CouponDetail = lazyLoad(import('@/pages/coupon-detail'));
 
-const Search = loadable(() => import('@/pages/search'), {
-  fallback: (
-    <div>
-      <Loading />
-    </div>
-  )
-});
+const Search = lazyLoad(import('@/pages/search'));
 
-const SearchResult = loadable(() => import('@/pages/search-result'), {
-  fallback: (
-    <div>
-      <Loading />
-    </div>
-  )
-});
+const SearchResult = lazyLoad(import('@/pages/search-result'));
 
-const User = loadable(() => import('@/pages/user'), {
-  fallback: (
-    <div>
-      <Loading />
-    </div>
-  )
-});
+const User = lazyLoad(import('@/pages/user'));
 
-const Purchase = loadable(() => import('@/pages/purchase'), {
-  fallback: (
-    <div>
-      <Loading />
-    </div>
-  )
-});
+const Purchase = lazyLoad(import('@/pages/purchase'));
 
 export default function Routers() {
   return (
